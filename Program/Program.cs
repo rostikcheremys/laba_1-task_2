ee019640f8749ee0ee184f43e473139b2080b636
rostikcheremys <rostikcheremys@gmail.com>
2024-02-22 19:41:07 +0200

    fix problems

--- Program/Program.cs
+++ Program/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Program
@@ -16,43 +17,30 @@ namespace Program
             
             IsMultiple isMultiple = (number, index) => number % k == 0;
             
-            int[] newArray = numbers.Where(number => isMultiple(number, k)).ToArray();
-            
-            Console.WriteLine($"1) Результат: {string.Join(" ", newArray)}");
-
-            MyMethod();
+            Console.WriteLine($"1) Результат: {string.Join(" ", FirstTask(numbers, k, isMultiple))}");
+            Console.WriteLine($"2) Результат: {string.Join(" ", SecondTask(numbers, k, isMultiple))}");
         }
 
-        private static void MyMethod()
+        private static int[] FirstTask(int[] numbers, int k, IsMultiple isMultiple)
         {
-            int[] numbers = { 5, 15, 25, 70, 95 };
-            int count = 0;
-            
-            Console.Write("\nВведіть значення k: ");
-            int k = Convert.ToInt32(Console.ReadLine());
-            
-            IsMultiple isMultiple = (number, index) => number % k == 0;
-            
+            int[] newArray = numbers.Where(number => isMultiple(number, k)).ToArray();
+
+            return newArray;
+        }
+        
+        private static int[] SecondTask(int[] numbers, int k, IsMultiple isMultiple)
+        {
+            List<int> newArray = new List<int>();
+
             foreach (int number in numbers)
             {
                 if (isMultiple(number, k))
                 {
-                    count++;
+                    newArray.Add(number);
                 }
             }
 
-            int[] newArray = new int[count];
-            int num = 0;
-            
-            foreach (int number in numbers)
-            {
-                if (isMultiple(number, k))
-                {
-                    newArray[num++] = number;
-                }
-            }
-            
-            Console.WriteLine($"2) Результат: {string.Join(" ", newArray)}");
+            return newArray.ToArray();
         }
     }
 }
